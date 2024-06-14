bb52b035d1846cd79da2ae0ff8841724994428a0
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-06-14 15:02:27 +0200

    section 2 variables

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,5 +1,13 @@
 public class HelloUniverse {
     public static void main(String... args)  {
-        System.out.println("Hello Universe!");
+        String message = "Aux dernieres nouvelles, le nombre total de planetes dans le systeme solaire est de :";
+        System.out.println(message);
+        int nbPlanetes = 8;
+        System.out.println(nbPlanetes);
+        message="Il y a quelques années cependant, elles étaient au nombre de : ";
+        System.out.println(message);
+        nbPlanetes = 9;
+        System.out.println(nbPlanetes);
+
     }
 }
