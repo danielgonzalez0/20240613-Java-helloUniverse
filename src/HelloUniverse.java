4e462ca5aa7441f78cfc8abcb91f49007a0529bf
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2024-06-19 17:59:13 +0200

    feat section 3(structure de controle):fin section structure de contrôle

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,3 +1,5 @@
+
+
 public class HelloUniverse {
     public static void main(String... args)  {
         // String message = "Aux dernieres nouvelles, le nombre total de planetes dans le systeme solaire est de : ";
@@ -7,14 +9,44 @@ public class HelloUniverse {
         // nbPlanetes++;
         // System.out.println(message + nbPlanetes);
 
-String message = "En %d, les planètes du système solaire était au nombre de : %d";
+String message = "En %d, les planètes du système solaire était au nombre de : %d. ";
+String error = "Le programme ne peut pas fournir de résultat pour l'année %d. ";
 
 short annee = 2013;
+int nbPlanetes;
 
-int nbPlanetes = annee < 2006 ? 9 : 8;
-
-
-
-System.out.printf(message, annee, nbPlanetes);
-    }
+if(annee < 1600 || annee > 2024) {
+System.out.printf(error + "\n", annee);
+return ;
 }
+
+if(annee < 1700) {
+nbPlanetes = 7;
+} else if(annee <= 1800) {
+nbPlanetes = 8; // Commented out the assignment statement
+}else if(annee < 2006) {
+    nbPlanetes = 9;
+}else {
+    nbPlanetes = 8;
+}
+System.out.printf(message + "\n", annee, nbPlanetes);
+
+//switch
+int newNBPlanetes = 7;
+
+//boucle for
+while (newNBPlanetes < 10) {
+    String messageToDisplay = switch (newNBPlanetes) {
+        case 7 -> "On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes";
+        case 8 -> "On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006";
+        case 9 -> "On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8";
+        default -> "Le programme ne peut pas fournir de résultat pour %d";
+    };
+       System.out.printf(messageToDisplay + "\n", newNBPlanetes);
+       newNBPlanetes++;
+}
+
+}//end public
+}
+
+
--- src/structuresDeControles/Structures.java
+++ src/structuresDeControles/Structures.java
@@ -24,6 +24,27 @@ public class Structures {
     //opérateur ternaire
 System.out.println(ageDuCapitaine > 20 && age  < 50 ? "L'âge du capitaine est de " + ageDuCapitaine + " ans": "le code ne permet pas d'afficer l'âge du capitaine");
 
-  }
+//structure conditionnelle switch
+
+int mois = 1;
+        String nomMois = switch (mois) {
+            case 1 -> "Janvier";
+            case 2 -> "Février";
+            case 3 -> "Mars";
+            case 4 -> "Avril";
+            case 5 -> "Mai";
+            case 6 -> "Juin";
+            case 7 -> "Juillet";
+            case 8 -> "Août";
+            case 9 -> "Septembre";
+            case 10 -> "Octobre";
+            case 11 -> "Novembre";
+            case 12 -> "Décembre";
+            default -> "Mois inconnu";
+        };
+
+        System.out.println(nomMois);
+}
 
 }
+
